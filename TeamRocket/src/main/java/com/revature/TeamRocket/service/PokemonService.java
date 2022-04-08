7461a9ce86aa2cad7df3918dcc7f8e8370c77908
JustinSmith <steelindian66@gmail.com>
2022-04-08 16:48:53 -0500

    Created Pokemon Service

--- TeamRocket/src/main/java/com/revature/TeamRocket/service/PokemonService.java
+++ TeamRocket/src/main/java/com/revature/TeamRocket/service/PokemonService.java
@@ -1,4 +1,16 @@
 package com.revature.TeamRocket.service;
 
+import com.revature.TeamRocket.repository.PokemonRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PokemonService {
+    private PokemonRepository pokemonRepository;
+
+    public PokemonService(PokemonRepository pokemonRepository){this.pokemonRepository = pokemonRepository;}
+
+    public Pokemon createNewPokemon(Pokemon pokemon){
+        return pokemonRepository.save(pokemon);
+    }
+    public List<Pokemon> getAllPokemon(){return pokemonRepository.findAll();}
 }
