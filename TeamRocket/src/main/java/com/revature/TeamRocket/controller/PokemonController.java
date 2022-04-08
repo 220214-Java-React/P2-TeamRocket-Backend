98e5f191da9657dce13b35f07468c17de05f2bd8
JustinSmith <steelindian66@gmail.com>
2022-04-08 16:55:30 -0500

    Created Pokemon Controller

--- TeamRocket/src/main/java/com/revature/TeamRocket/controller/PokemonController.java
+++ TeamRocket/src/main/java/com/revature/TeamRocket/controller/PokemonController.java
@@ -1,4 +1,24 @@
 package com.revature.TeamRocket.controller;
 
+import com.revature.TeamRocket.model.Pokemon;
+import com.revature.TeamRocket.service.PokemonService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Pokemon")
 public class PokemonController {
+
+    private PokemonService pokemonService;
+
+    public PokemonController(PokemonService pokemonService) {this.pokemonService = pokemonService;}
+
+    @GetMapping
+    public List<Pokemon> getAllPokemon() {return pokemonService.getAllPokemon();}
+
+    @PostMapping
+    public Pokemon createPokemon(@RequestBody Pokemon pokemon){
+        return pokemonService.createNewPokemon(pokemon);
+    }
 }
