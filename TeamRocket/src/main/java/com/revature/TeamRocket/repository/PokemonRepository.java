5e86a32aebf245204276d12e698b0c86e4af064c
JustinSmith <steelindian66@gmail.com>
2022-04-08 16:50:53 -0500

    Create Pokemon Repository

--- TeamRocket/src/main/java/com/revature/TeamRocket/repository/PokemonRepository.java
+++ TeamRocket/src/main/java/com/revature/TeamRocket/repository/PokemonRepository.java
@@ -1,4 +1,9 @@
 package com.revature.TeamRocket.repository;
 
-public interface PokemonRepository {
+import com.revature.TeamRocket.model.Pokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PokemonRepository extends JpaRepository<Pokemon, Integer> {
 }
