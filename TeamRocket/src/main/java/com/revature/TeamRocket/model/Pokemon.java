8d31f81e727b4ec13629890344eb1ecfe55a43e9
JustinSmith <steelindian66@gmail.com>
2022-04-08 16:45:44 -0500

    Create Pokemon model

--- TeamRocket/src/main/java/com/revature/TeamRocket/model/Pokemon.java
+++ TeamRocket/src/main/java/com/revature/TeamRocket/model/Pokemon.java
@@ -1,4 +1,19 @@
 package com.revature.TeamRocket.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pokemon")
+@Data
 public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String Pokemon;
+
+    @ManyToOne@JoinColumn(name ="userName", nullable = false)
+    private User user;
 }
